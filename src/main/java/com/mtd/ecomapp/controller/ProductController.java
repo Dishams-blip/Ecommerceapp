32c05c541f57dc28febdc19ac00207624c3fad0f
Dishams-blip <dishasridhara1@gmail.com>
2025-09-17 11:13:41 +0530

    new

--- src/main/java/com/mtd/ecomapp/controller/ProductController.java
+++ src/main/java/com/mtd/ecomapp/controller/ProductController.java
@@ -1,5 +1,4 @@
-package com.mtd.ecomapp.controller;
-
+package com.proj1.EcomApp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,34 +11,34 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mtd.ecomapp.entity.Product;
-import com.mtd.ecomapp.service.Productservice;
+import com.proj1.EcomApp.entity.Product;
+import com.proj1.EcomApp.service.ProductService;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/Products")
 public class ProductController {
 	@Autowired
-	private Productservice productService;
-	
+	private ProductService productService;
 	@PostMapping("/save")
 	public Product save(@RequestBody Product product) {
 		return productService.saveProduct(product);
+		
 	}
-	
 	@GetMapping("/{id}")
 	public Product findById(@PathVariable String id) {
-			return productService.getProductById(id);
-		}
+		return productService.getProductById(id);
+	}
 	@GetMapping("/all")
 	public List<Product> findAll(){
 		return productService.getProducts();
 	}
 	@PutMapping("/{id}")
-	public Product updateProduct(@RequestBody Product product,@PathVariable String id) {
-		return productService.updateProduct(product,id);
+	public Product updateproduct(@RequestBody Product product,@PathVariable String id) {
+		return productService.updateProduct(product, id);
 	}
 	@DeleteMapping("/{id}")
 	public boolean deleteProduct(@PathVariable String id) {
 		return productService.deleteProduct(id);
 	}
+
 }
